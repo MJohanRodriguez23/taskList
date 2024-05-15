adf0fb5ae3e355beb094c9da184000aff2d03162
Miguel Rodriguez <mjrodrigueza@ufpso.edu.co>
2024-05-15 13:16:33 -0500

    first commit

---
+++ src/components/Vue/Directives.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+let msg = ref('Soy un tooltip')
+let checked = ref('false')
+
+let data =["hola", "mundo", "bonito"]
+
+</script>
+
+<template>
+    <input class="border" :class="{'border-red-500': checked}" v-model="msg" />
+    <div :title="msg">
+        {{ msg }}
+    </div>
+    <label for="check">seleccionado {{ checked }}</label>
+    <input type="checkbox" v-model="checked">
+
+    <ul>
+        <li v-for="(text, index) in data" :key="index">{{ text }}</li>
+    </ul>
+</template>
